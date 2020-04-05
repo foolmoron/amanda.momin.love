81642a8bf1d5d4a2c61a084da8d25cd738f24ad1
Momin Khan <foolmoron@gmail.com>
2020-04-05 17:22:02 -0400

    Most layout blocked out

--- main.js
+++ main.js
@@ -7,11 +7,22 @@ function ilerp(x, a, b) {
 }
 
 // callout banner color anim
+function doLetters(letters) {
+    letters.forEach(letter => {
+        letter.style.transform = `translateX(${lerp(-1.5, 1.5, Math.random())}%) translateY(${lerp(-2.5, 2.5, Math.random())}%) rotate(${lerp(-9, 9, Math.random())}deg)`;
+        letter.style.setProperty('--color-angle', `${lerp(0, 360, Math.random())}deg`);
+        letter.style.backgroundPosition = `${lerp(-100, 100, Math.random())}% ${lerp(-100, 100, Math.random())}%`;
+    });
+};
 document.querySelectorAll('.callout').forEach(callout => {
+    const letters = callout.querySelectorAll('.title span');
+    let intervalId = 0;
     callout.addEventListener('mouseenter', e => {
-        callout.style.setProperty('--color-angle', lerp(15, 75, Math.random()) + 'deg');
+        intervalId = setInterval(() => doLetters(letters), 450);
+        doLetters(letters);
     });
     callout.addEventListener('mouseleave', e => {
-        callout.style.setProperty('--color-angle', -lerp(15, 75, Math.random()) + 'deg');
+        clearInterval(intervalId);
+        letters.forEach(letter => letter.style.transform = ``);
     });
 });
