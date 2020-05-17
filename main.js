6a15fcc62ad737110bdc4ac9308534b6765b24b3
Momin Khan <foolmoron@gmail.com>
2020-05-17 16:34:04 -0400

    full drawing flow done (for mobile)

--- main.js
+++ main.js
@@ -21,6 +21,9 @@ function ilerp(x, a, b) {
     return (x - a) / (b - a)
 }
 
+// config
+const DRAWING_POST_URL = 'http://localhost:8000'
+
 // callout banner color anim
 function doLetters(letters) {
     letters.forEach(letter => {
@@ -41,11 +44,6 @@ document.querySelectorAll('.callout').forEach(callout => {
         letters.forEach(letter => letter.style.transform = ``)
     })
 })
-
-// callbacks
-function submitFullName(name) {
-    console.log('SUBMIT', name)
-}
 
 // main
 const canvas = new fabric.Canvas('canvas', {
@@ -111,3 +109,147 @@ for (let button of widthButtons) {
     button.onclick = function(e) { setWidth(e.target) }
 }
 setWidth(widthButtons[1])
+
+// drawing history
+let history = []
+let historyIndex = 0
+let doingHistory = false
+let justCleared = false
+function redrawUpToHistory(index) {
+    justCleared = false
+    canvas.clear()
+    doingHistory = true
+    for (let i = 0; i < index && i < history.length; i++) {
+        canvas.add(history[i])
+    }
+    doingHistory = false
+    canvas.renderAll()
+}
+
+// handle when new path is drawn
+canvas.on('object:added', function(e) {
+    if (doingHistory) return
+
+    justCleared = false
+    if (historyIndex < history.length) {
+        history = history.slice(0, historyIndex)
+    }
+    history.push(e.target)
+    historyIndex++
+
+    // reset submitted state
+    submitState = SUBMIT_STATE.NONE
+
+    // save drawing
+    localStorage.setItem('DRAW-drawing', JSON.stringify(canvas.toObject()))
+})
+
+// load drawing after page load
+document.addEventListener('DOMContentLoaded', e => {
+    canvas.loadFromJSON(JSON.parse(localStorage.getItem('DRAW-drawing')), function() {
+        canvas.renderAll()
+    })
+})
+
+// drawing controls
+const undoButton = document.querySelector('.undo')
+const redoButton = document.querySelector('.redo')
+const clearButton = document.querySelector('.clear')
+
+undoButton.onclick = function(e) {
+    if (justCleared) {
+        historyIndex = history.length
+        redrawUpToHistory(historyIndex, true)
+    } else if (historyIndex > 0) {
+        historyIndex = Math.max(historyIndex - 1, 0)
+        redrawUpToHistory(historyIndex, true)
+    }
+}
+redoButton.onclick = function(e) {
+    if (justCleared) {
+        historyIndex = history.length
+        redrawUpToHistory(historyIndex, true)
+    } else if (historyIndex < history.length) {
+        historyIndex = Math.min(historyIndex + 1, history.length)
+        redrawUpToHistory(historyIndex, true)
+    }
+}
+clearButton.onclick = function(e) {
+    if (historyIndex > 0) {
+        historyIndex = 0
+        redrawUpToHistory(historyIndex, true)
+        justCleared = true
+        // flash canvas red
+        const canvasContainer = document.querySelector('.canvas-container')
+        const prevTransition = canvasContainer.style.transition
+        const prevColor = canvasContainer.style.backgroundColor
+        canvasContainer.style.transition = 'none'
+        canvasContainer.style.backgroundColor = '#ff5151'
+        setTimeout(() => {
+            canvasContainer.style.transition = prevTransition
+            canvasContainer.style.backgroundColor = prevColor
+        }, 0)
+    }
+}
+
+doEachAnimationFrame(() => {
+    undoButton.classList.toggle('disabled', !justCleared && historyIndex <= 0 && canvas.isDrawingMode)
+    redoButton.classList.toggle('disabled', !justCleared && historyIndex >= history.length && canvas.isDrawingMode)
+    clearButton.classList.toggle('disabled', canvas._objects.length == 0 && canvas.isDrawingMode)
+})
+
+// drawing submit
+const SUBMIT_STATE = {
+    NONE: 'NONE',
+    INPROGRESS: 'INPROGRESS',
+    FAILED: 'FAILED',
+    DONE: 'DONE',
+}
+let submitState = SUBMIT_STATE.NONE
+function submitDrawing() {
+    submitState = SUBMIT_STATE.INPROGRESS
+    // calculate and cache bounds
+    const group = new fabric.Group(canvas.getObjects(), null, true)
+    group._calcBounds()
+    const bounds = {
+        left: group.left,
+        top: group.top,
+        width: group.width,
+        height: group.height,
+    }
+    // reset objects to center
+    group.setPositionByOrigin({x: 0, y: 0})
+    // get data and shift objects to top-left based on bounds
+    const data = canvas.toObject()
+    data.objects.forEach(function(obj) {
+        obj.left = obj.left - bounds.left
+        obj.top = obj.top - bounds.top
+    })
+    // post data and dimensions
+    fetch(DRAWING_POST_URL, {
+        method: 'post',
+        body: JSON.stringify({
+            name: nameInput.value || '',
+            json: data,
+            dimensions: { width: Math.ceil(bounds.width), height: Math.ceil(bounds.height) },
+        })
+    }).then(res => {
+        submitState = SUBMIT_STATE.DONE
+    }).catch(err => {
+        submitState = SUBMIT_STATE.FAILED
+    })
+}
+
+// submit buttons and drawing ability based on submit state
+const submitButtons = {
+    [SUBMIT_STATE.NONE]: document.querySelector('.submit-none'),
+    [SUBMIT_STATE.INPROGRESS]: document.querySelector('.submit-inprogress'),
+    [SUBMIT_STATE.FAILED]: document.querySelector('.submit-failed'),
+    [SUBMIT_STATE.DONE]: document.querySelector('.submit-done'),
+}
+const submitButtonsAll = Object.values(submitButtons)
+doEachAnimationFrame(() => {
+    submitButtonsAll.forEach(b => b.style.display = 'none')
+    submitButtons[submitState].style.display = null
+    canvas.wrapperEl.classList.toggle('disable-draw', submitState === SUBMIT_STATE.INPROGRESS)
+})
